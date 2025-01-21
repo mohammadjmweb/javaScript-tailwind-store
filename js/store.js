0b64e1d1599ff05f0ebc55451165b0c50e964689
mohammadjmweb <mohammadjmweb@gmail.com>
2025-01-21 15:06:17 +0330

    create products object

---
+++ js/store.js
@@ -0,0 +1,27 @@
+let products=[
+    {
+        id:'product1',
+        name:'flash memory AData 64gb',
+        price:11.99,
+        img:"../images/adata64.jpg"
+    },
+    {
+        id:'product2',
+        name:'flash memory SanDisk 128gb',
+        price:12.99,
+        img:'../images/sandisk128.jpg'
+    },
+    {
+        id:'product3',
+        name:'airpod Oraimo',
+        price:29.99,
+        img:'../images/airpodOraimo.jpg'
+    },
+    {
+        id:'product4',
+        name:'airpod QCY',
+        price:26.99,
+        img:'../images/airpodQCY.jpg'
+    }
+]
+
